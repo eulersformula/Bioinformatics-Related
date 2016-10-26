5c9224c926a65a469f732801f9a2b958787b257b
Rinnegan <gaofeng.fdu@gmail.com>
2016-10-25 22:19:57 -0400

    Update vcf_to_plink.sh

--- vcf_to_plink.sh
+++ vcf_to_plink.sh
@@ -9,6 +9,13 @@ for i in $(seq 22); do
   ~/tools/plink2 --vcf $fname --make-bed --maf 0.05 --snps-only --biallelic-only --out 'alspac_'$i 
 done
 
+#renaming name of SNPs without rsid
+for i in $(seq 22); do
+  cat 'alspac_'$i'.bim' | awk '{if ($2 == ".") $2 = "chr"$1"_"$4; print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6}' > 'alspac_'$i'.new.bim'
+  mv 'alspac_'$i'.bim' 'alspac_'$i'.old.bim'
+  mv 'alspac_'$i'.new.bim' 'alspac_'$i'.bim'
+done
+
 #combine the 22 chromosomes
 rm alspac_mergelist.txt
 touch alspac_mergelist.txt
@@ -17,13 +24,19 @@ for i in $(seq 22); do
 done
 ~/tools/plink2 --make-bed --merge-list alspac_mergelist.txt --out alspac_all
 
-#just another dataset
+########################just another dataset##############################
 
 base=6607
 for i in $(seq 22); do
   num=$(echo $base+$i | bc)
   fname='/bscb/ak735_0001/data/XWAS/temp/twins/_EGAZ0000101'$num'_'$i'.TWINSUK.beagle.anno.csq.shapeit.20131101.vcf.gz'
   ~/tools/plink2 --bcf $fname --make-bed --maf 0.05 --snps-only --biallelic-only --out 'twins_'$i
+done
+
+for i in $(seq 22); do
+  cat 'twins_'$i'.bim' | awk '{if ($2 == ".") $2 = "chr"$1"_"$4; print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6}' > 'twins_'$i'.new.bim'
+  mv 'twins_'$i'.bim' 'twins_'$i'.old.bim'
+  mv 'twins_'$i'.new.bim' 'twins_'$i'.bim'
 done
 
 rm twins_mergelist.txt
